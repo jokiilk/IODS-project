a6bcf9ff21b0eb5fcada1e64cd5d368aa95e462a
jokiilk <ilkka.jokipii@helsinki.fi>
2017-01-26 16:06:13 +0200

    versio

--- data/create_learning2014.R
+++ data/create_learning2014.R
@@ -1,1 +1,11 @@
-#Ilkka Jokipii, 26.1.2017
+#Ilkka Jokipii, 26.1.2017, Learning data exercises
+
+learn <- read.table("http://www.helsinki.fi/~kvehkala/JYTmooc/JYTOPKYS3-data.txt", sep="\t", header=TRUE)
+
+dim(learn)
+str(learn)
+#Learning data has 183 observations and 60 variables. Table has 183 rows and 60 columns. Gender is the only variable that is not in integer format
+
+install.packages("dplyr")
+library("dplyr")
+R.version
